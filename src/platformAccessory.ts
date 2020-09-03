9615324c16fb5717452b7a921780f1be32f5bac0
Aurimas Niekis <aurimas@niekis.lt>
2020-09-03 22:19:45 +0900

    All code changes i have made

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,151 +1,198 @@
-import { Service, PlatformAccessory, CharacteristicValue, CharacteristicSetCallback, CharacteristicGetCallback } from 'homebridge';
+import {
+  Service,
+  PlatformAccessory,
+  CharacteristicValue,
+  CharacteristicSetCallback,
+  CharacteristicGetCallback,
+  Characteristic,
+} from 'homebridge';
 
-import { ExampleHomebridgePlatform } from './platform';
+import {PanasonicEoliaPlatform} from './platform';
+import {EoliaAirConditioner, OperationMode} from 'eolia-client';
 
 /**
  * Platform Accessory
  * An instance of this class is created for each accessory your platform registers
  * Each accessory may expose multiple services of different service types.
  */
-export class ExamplePlatformAccessory {
-  private service: Service;
+export class PanasonicEoliaAccessory {
+    private service: Service;
+    private readonly Characteristic: typeof Characteristic;
+    private readonly device: EoliaAirConditioner;
 
-  /**
-   * These are just used to create a working example
-   * You should implement your own code to track the state of your accessory
-   */
-  private exampleStates = {
-    On: false,
-    Brightness: 100,
-  }
+    constructor(
+        private readonly platform: PanasonicEoliaPlatform,
+        private readonly accessory: PlatformAccessory,
+    ) {
 
-  constructor(
-    private readonly platform: ExampleHomebridgePlatform,
-    private readonly accessory: PlatformAccessory,
-  ) {
+      this.device = accessory.context.device;
+      this.Characteristic = this.platform.Characteristic;
 
-    // set accessory information
-    this.accessory.getService(this.platform.Service.AccessoryInformation)!
-      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Default-Manufacturer')
-      .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
-      .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
+        // set accessory information
+        this.accessory.getService(this.platform.Service.AccessoryInformation)!
+          .setCharacteristic(this.Characteristic.Manufacturer, 'Panasonic')
+          .setCharacteristic(this.Characteristic.Model, this.accessory.context.device.productCode)
+          .setCharacteristic(this.Characteristic.SerialNumber, this.accessory.context.device.applianceId);
 
-    // get the LightBulb service if it exists, otherwise create a new LightBulb service
-    // you can create multiple services for each accessory
-    this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
+        // get the LightBulb service if it exists, otherwise create a new LightBulb service
+        // you can create multiple services for each accessory
+        this.service = this.accessory.getService(this.platform.Service.HeaterCooler) ||
+            this.accessory.addService(this.platform.Service.HeaterCooler);
 
-    // set the service name, this is what is displayed as the default name on the Home app
-    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
-    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.exampleDisplayName);
+        // set the service name, this is what is displayed as the default name on the Home app
+        // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
+        this.service.setCharacteristic(this.Characteristic.Name, accessory.context.nickname)
+          .setCharacteristic(this.Characteristic.Active, this.mapActive())
+          .setCharacteristic(this.Characteristic.CurrentHeaterCoolerState, this.mapCurrentHeaterCoolerState())
+          .setCharacteristic(this.Characteristic.TargetHeaterCoolerState, this.mapTargetHeaterCoolerState())
+          .setCharacteristic(this.Characteristic.CurrentTemperature, this.mapCurrentTemperature());
 
-    // each service must implement at-minimum the "required characteristics" for the given service type
-    // see https://developers.homebridge.io/#/service/Lightbulb
+        this.service.getCharacteristic(this.Characteristic.Active)
+          .on('get', this.getActive.bind(this))
+          .on('set', this.setActive.bind(this));
 
-    // register handlers for the On/Off Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.On)
-      .on('set', this.setOn.bind(this))                // SET - bind to the `setOn` method below
-      .on('get', this.getOn.bind(this));               // GET - bind to the `getOn` method below
+        this.service.getCharacteristic(this.Characteristic.CurrentHeaterCoolerState)
+          .on('get', this.getCurrentHeaterCoolerState.bind(this));
 
-    // register handlers for the Brightness Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-      .on('set', this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
+        this.service.getCharacteristic(this.Characteristic.CurrentTemperature)
+          .on('get', this.getCurrentTemperature.bind(this));
 
+        this.service.getCharacteristic(this.Characteristic.TargetHeaterCoolerState)
+          .on('get', this.getTargetHeaterCoolerState.bind(this))
+          .on('set', this.setTargetHeaterCoolerState.bind(this));
+    }
 
-    /**
-     * Creating multiple services of the same type.
-     * 
-     * To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
-     * when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
-     * this.accessory.getService('NAME') || this.accessory.addService(this.platform.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE_ID');
-     * 
-     * The USER_DEFINED_SUBTYPE must be unique to the platform accessory (if you platform exposes multiple accessories, each accessory
-     * can use the same sub type id.)
-     */
+    getActive(callback: CharacteristicGetCallback) {
+      callback(null, this.mapActive());
+    }
 
-    // Example: add two "motion sensor" services to the accessory
-    const motionSensorOneService = this.accessory.getService('Motion Sensor One Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor One Name', 'YourUniqueIdentifier-1');
+    mapActive() {
+      return this.device.operationStatus ? 1 : 0;
+    }
 
-    const motionSensorTwoService = this.accessory.getService('Motion Sensor Two Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor Two Name', 'YourUniqueIdentifier-2');
+    async setActive(value: CharacteristicValue, callback: CharacteristicSetCallback) {
+      this.device.operationStatus = !!value;
 
-    /**
-     * Updating characteristics values asynchronously.
-     * 
-     * Example showing how to update the state of a Characteristic asynchronously instead
-     * of using the `on('get')` handlers.
-     * Here we change update the motion sensor trigger states on and off every 10 seconds
-     * the `updateCharacteristic` method.
-     * 
-     */
-    let motionDetected = false;
-    setInterval(() => {
-      // EXAMPLE - inverse the trigger
-      motionDetected = !motionDetected;
+      this.platform.log.debug('Set Characteristic Active ->', value);
+      console.log(this.service.getCharacteristic(this.Characteristic.TargetHeaterCoolerState).value);
+      this.device.operationMode = this.mapValueTargetHeaterCoolerState(this.service.getCharacteristic(this.Characteristic.TargetHeaterCoolerState).value ?? this.Characteristic.TargetHeaterCoolerState.AUTO);
 
-      // push the new value to HomeKit
-      motionSensorOneService.updateCharacteristic(this.platform.Characteristic.MotionDetected, motionDetected);
-      motionSensorTwoService.updateCharacteristic(this.platform.Characteristic.MotionDetected, !motionDetected);
+      await this.applySettings(callback);
+    }
 
-      this.platform.log.debug('Triggering motionSensorOneService:', motionDetected);
-      this.platform.log.debug('Triggering motionSensorTwoService:', !motionDetected);
-    }, 10000);
-  }
+    getCurrentHeaterCoolerState(callback: CharacteristicGetCallback) {
+      callback(null, this.mapCurrentHeaterCoolerState());
+    }
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
-   */
-  setOn(value: CharacteristicValue, callback: CharacteristicSetCallback) {
+    mapCurrentHeaterCoolerState() {
+      const currentRoomTemp = this.device.insideTemperature;
+      const targetTemp = this.device.temperature;
 
-    // implement your own code to turn your device on/off
-    this.exampleStates.On = value as boolean;
+      if (false === this.device.operationStatus) {
+        return this.Characteristic.CurrentHeaterCoolerState.INACTIVE;
+      }
 
-    this.platform.log.debug('Set Characteristic On ->', value);
+      switch (this.device.operationMode) {
+        case OperationMode.HEATING:
+          if (currentRoomTemp < targetTemp) {
+            return this.Characteristic.CurrentHeaterCoolerState.HEATING;
+          } else {
+            return this.Characteristic.CurrentHeaterCoolerState.IDLE;
+          }
+        case OperationMode.COOLING:
+          if (currentRoomTemp > targetTemp) {
+            return this.Characteristic.CurrentHeaterCoolerState.COOLING;
+          } else {
+            return this.Characteristic.CurrentHeaterCoolerState.IDLE;
+          }
+        case OperationMode.BLAST:
+          if (currentRoomTemp > targetTemp) {
+            return this.Characteristic.CurrentHeaterCoolerState.COOLING;
+          } else {
+            return this.Characteristic.CurrentHeaterCoolerState.IDLE;
+          }
+        case OperationMode.COOL_DEHUMIDIFYING:
+          if (currentRoomTemp > targetTemp) {
+            return this.Characteristic.CurrentHeaterCoolerState.COOLING;
+          } else {
+            return this.Characteristic.CurrentHeaterCoolerState.IDLE;
+          }
+        case OperationMode.AUTO:
+          if (currentRoomTemp > (targetTemp - 1)) {
+            return this.Characteristic.CurrentHeaterCoolerState.COOLING;
+          } else if (currentRoomTemp < (targetTemp - 1)) {
+            return this.Characteristic.CurrentHeaterCoolerState.HEATING;
+          } else {
+            return this.Characteristic.CurrentHeaterCoolerState.IDLE;
+          }
+      }
 
-    // you must call the callback function
-    callback(null);
-  }
+      return this.Characteristic.CurrentHeaterCoolerState.INACTIVE;
+    }
 
-  /**
-   * Handle the "GET" requests from HomeKit
-   * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
-   * 
-   * GET requests should return as fast as possbile. A long delay here will result in
-   * HomeKit being unresponsive and a bad user experience in general.
-   * 
-   * If your device takes time to respond you should update the status of your device
-   * asynchronously instead using the `updateCharacteristic` method instead.
+    getTargetHeaterCoolerState(callback: CharacteristicGetCallback) {
+      callback(null, this.mapTargetHeaterCoolerState());
+    }
 
-   * @example
-   * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
-   */
-  getOn(callback: CharacteristicGetCallback) {
+    mapTargetHeaterCoolerState() {
+      if (false === this.device.operationStatus) {
+        return this.Characteristic.TargetHeaterCoolerState.AUTO;
+      }
 
-    // implement your own code to check if the device is on
-    const isOn = this.exampleStates.On;
+      switch (this.device.operationMode) {
+        case OperationMode.HEATING:
+          return this.Characteristic.TargetHeaterCoolerState.HEAT;
+        case OperationMode.COOLING:
+          return this.Characteristic.TargetHeaterCoolerState.COOL;
+        case OperationMode.BLAST:
+          return this.Characteristic.TargetHeaterCoolerState.COOL;
+        case OperationMode.COOL_DEHUMIDIFYING:
+          return this.Characteristic.TargetHeaterCoolerState.COOL;
+        case OperationMode.AUTO:
+          return this.Characteristic.TargetHeaterCoolerState.AUTO;
+      }
 
-    this.platform.log.debug('Get Characteristic On ->', isOn);
+      return this.Characteristic.TargetHeaterCoolerState.AUTO;
+    }
 
-    // you must call the callback function
-    // the first argument should be null if there were no errors
-    // the second argument should be the value to return
-    callback(null, isOn);
-  }
+    mapValueTargetHeaterCoolerState(value: CharacteristicValue) {
+      switch (value) {
+        case this.Characteristic.TargetHeaterCoolerState.HEAT:
+          return OperationMode.HEATING;
+        case this.Characteristic.TargetHeaterCoolerState.COOL:
+          return OperationMode.COOLING;
+        case this.Characteristic.TargetHeaterCoolerState.AUTO:
+        default:
+          return OperationMode.AUTO;
+      }
+    }
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, changing the Brightness
-   */
-  setBrightness(value: CharacteristicValue, callback: CharacteristicSetCallback) {
+    async setTargetHeaterCoolerState(value: CharacteristicValue, callback: CharacteristicSetCallback) {
+      this.device.operationMode = this.mapValueTargetHeaterCoolerState(value);
 
-    // implement your own code to set the brightness
-    this.exampleStates.Brightness = value as number;
+      this.platform.log.debug('Set Characteristic TargetHeaterCoolerState ->', this.device.operationMode);
 
-    this.platform.log.debug('Set Characteristic Brightness -> ', value);
+      await this.applySettings(callback);
+    }
 
-    // you must call the callback function
-    callback(null);
-  }
+    getCurrentTemperature(callback: CharacteristicGetCallback) {
+      callback(null, this.mapCurrentTemperature());
+    }
 
+    mapCurrentTemperature() {
+      return this.device.insideTemperature;
+    }
+
+    async applySettings(callback: CharacteristicSetCallback) {
+      try {
+        await this.platform.client.apply(this.device);
+
+        callback(null);
+      } catch (e) {
+        console.log(e);
+        this.platform.log.error('Error while applying AC state %s', JSON.stringify(e.response.data));
+
+        callback(e);
+      }
+    }
 }
